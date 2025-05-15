01564f70cd70de879a28646ebf553c1e628b5f66
Guilherme <guilherme_souzamcd@hotmail.com>
2025-05-15 14:31:18 -0300

    feat: Criei a IAService para simular a resposta da IA

--- Controllers/ViagemController.cs
+++ Controllers/ViagemController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using APIViagem.Models; // Aqui estou importando todos os arquivos da pasta Models.
+using APIViagem.Services; // Importando a pasta Services. 
 
 namespace APIViagem.Controllers
 {
@@ -10,10 +12,24 @@ namespace APIViagem.Controllers
     [Route("api/[controller]")]
     public class ViagemController : ControllerBase
     {
-     [HttpGet]
-     public string TesteConexao()
-     {
-        return "API OK!";
-     }   
+        private readonly IAService _iaService; // Crio uma variavel privada chamada _iaService que vem lá do meu arquivo IAService
+
+        public ViagemController()
+        {
+            _iaService = new IAService(); // Instancia da classe para poder usar os metodos do controller
+        }
+
+        [HttpGet]
+        public string TesteConexao()
+        {
+            return "API OK!"; // Mensagem de teste para saber se a API está funcionando
+        }
+
+        [HttpPost("calcular-rota")]
+        public IActionResult CalcularRota([FromBody] DadosViagem dados)
+        {
+            var resultado = _iaService.ObterResultadoDaIA(dados); // Chama o método do serviço passando os dados da viagem
+            return Ok(resultado); // Retorna o resultado da IA como resposta
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers(); // Criar o service dos controllers
 
 var app = builder.Build();
 
@@ -13,4 +12,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers(); // Adiciona os controllers
 app.Run();
